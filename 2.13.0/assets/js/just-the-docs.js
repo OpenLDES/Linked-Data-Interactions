d61a68ecd4fa39ef4b2f49f3d4c213676b32f5ee
Stijn Deknudt <stijn@ebisi.be>
2025-12-12 17:14:52 +0100

    Updated documentation for openldes.org

--- 2.13.0/assets/js/just-the-docs.js
+++ 2.13.0/assets/js/just-the-docs.js
@@ -55,7 +55,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', '/VSDS-Linked-Data-Interactions/2.13.0/assets/js/search-data.json', true);
+  request.open('GET', '/Linked-Data-Interactions/2.13.0/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
@@ -434,7 +434,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', '/VSDS-Linked-Data-Interactions/2.13.0/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', '/Linked-Data-Interactions/2.13.0/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Scroll site-nav to ensure the link to the current page is visible
